fe428466f996c28dc1009f9fe6cfd3ed4d0cbad2
BRC-Thomas <thomasbaracani@gmail.com>
2023-10-23 17:37:44 +0200

    Initial State

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,10 @@ import {
 } from "lucide-react";
 import UpdateTimeButton from "./components/UpdateTimeButton";
 import ToggleButton from "./components/ToggleButton";
+import { useAppSelector } from "./hooks";
 
 function App() {
+  const chronoValues = useAppSelector((state) => state.chrono);
   return (
     <div className="bg-black text-slate-100 pt-20 min-h-screen">
       <div className="max-w-xl mx-auto shadow-[#000] shadow-2xl rounded-2xl p-10">
@@ -50,7 +52,9 @@ function App() {
                 <p className="text-center mb-1">Sessions</p>
                 <div className="flex">
                   <UpdateTimeButton sign={"-"} type={"session"} />
-                  <p className="mx-3 text-xl">25</p>
+                  <p className="mx-3 text-xl">
+                    {chronoValues.session.value / 60}
+                  </p>
                   <UpdateTimeButton sign={"+"} type={"session"} />
                 </div>
               </div>
@@ -62,20 +66,26 @@ function App() {
                 <p className="text-center mb-1">Pauses</p>
                 <div className="flex">
                   <UpdateTimeButton sign={"-"} type={"pause"} />
-                  <p className="mx-3 text-xl">5</p>
+                  <p className="mx-3 text-xl">
+                    {chronoValues.pause.value / 60}
+                  </p>
                   <UpdateTimeButton sign={"+"} type={"pause"} />
                 </div>
               </div>
             </div>
 
-            <p className="text-center mb-2 text-xl font-semibold">Timer</p>
+            <p className="text-center mb-2 text-xl font-semibold">
+              {chronoValues.displayedValue.heading}
+            </p>
 
             <p className="text-center flex justify-center mb-1">
               <span className="text-4xl p-4 rounded bg-slate-300 text-black">
                 25:00
               </span>
             </p>
-            <p className="mb-10 text-center">Passed cycle(s) : 0</p>
+            <p className="mb-10 text-center">
+              Passed cycle(s) : {chronoValues.cycles}
+            </p>
             <ToggleButton />
           </section>
         </div>
